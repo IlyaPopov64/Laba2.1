4d1b77516a1d812e69359b9d1bb9c9541b7925d2
IlyaPopov64 <144610544+IlyaPopov64@users.noreply.github.com>
2023-09-25 09:54:27 +0700

    Laba2.1
    Laba2.1

---
+++ Laba2.1/Laba2.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	setlocale(LC_ALL, "ru");
+	cout << "Введите переменную a:" << endl;
+	cin >> a;
+	cout << "Введите переменную b:" << endl;
+	cin >> b;
+	printf("%g", sqrt(a * b));
+	return 0;
+}
